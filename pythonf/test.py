01cc8b241020df4697cd7af193fb79f754dc6615
Divya Prakash <divyaprakashara09@gmail.com>
2024-04-12 06:06:40 +0530

    some modification done in signup.html, login.html, and their css part and creating profile page

--- pythonf/test.py
+++
@@ -1,3 +0,0 @@
-#! C:\Users\divya\AppData\Local\Programs\Python\Python312\python.exe
-print ("Content-Type: text/html\r\n\r\n")
-print('Hello world sfdsfdgf')
--- scripts/signup.js
+++ scripts/signup.js
@@ -3,8 +3,42 @@ loginbtn.addEventListener("click", function () {
     window.location.href = 'login.html';
 })
 
-let subbtn = document.querySelector("#subbtn");
-subbtn.addEventListener("click", function (e) {
-    alert("Are you sure to submit this form ?")
-    e.preventDefault();
-})
+// let subbtn = document.querySelector("#subbtn");
+// subbtn.addEventListener("click", function (e) {
+//     alert("Are you sure to submit this form ?")
+//     e.preventDefault();
+// })
+
+$(function () {
+    $('#subbtn').click(function (e) {
+        e.preventDefault()
+        userid = $('#userid').val();
+        ufname = $("#ufname").val();
+        ulname = $("#ulname").val();
+        useremail = $('#uemail').val();
+        mobno = $('#mobno').val();
+        upass = $('#upass').val();
+        urpass = $('#urpass').val();
+        uagree = $('#uagree').val();
+        // alert(userid)
+        $.ajax({
+            method: 'post',
+            data: {
+                userid1: userid,
+                ufname1: ufname,
+                ulname1: ulname,
+                useremail1: useremail,
+                mobno1: mobno,
+                upass1: upass,
+                urpass1: urpass,
+                uagree1: uagree,
+            },
+            url: 'pythonf/signup.py',
+            success: function (data) {
+                // alert(data)
+                console.log(data);
+                console.log(typeof data);
+            }
+        });
+    });
+});
