960d30635a9b65f0ac1b67ffbd280f47600fa5d1
Divya Prakash <divyaprakashara09@gmail.com>
2024-04-15 14:54:43 +0530

    doing some modification on form part

--- scripts/forgetpassword.js
+++ scripts/forgetpassword.js
@@ -1,24 +1,23 @@
-$(function(){
+$(function () {
     // $('input[type="text"]').click(function(){
     //     $(this).siblings('.lab').css({'top':'0rem'})
     // })
-    $('input[type="email"]').click(function(){
-        $(this).siblings('.lab').css({'top':'-.6rem'});
+    $('input[type="email"]').click(function () {
+        $(this).siblings('.lab').css({ 'top': '-.6rem' });
     })
-    $('input[type="text"]').click(function(){
-        $(this).siblings('.lab').css({'top':'-.6rem'});
+    $('input[type="text"]').click(function () {
+        $(this).siblings('.lab').css({ 'top': '-.6rem' });
     })
 
-   
-
-    $('#loginbtn').on("click",function(){
+    $('#loginbtn').on("click", function () {
         window.location.href = "login.html";
     })
 
 })
 
-$(function(){
-    $('#getotp').on("click",function(){
+$(function () {
+    $('#getotp').on("click", function (e) {
+        e.preventDefault();
         window.location.href = "updatepassword.html";
     })
 })
